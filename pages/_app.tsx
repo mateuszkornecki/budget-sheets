ecfc7a99cbac0a3c36e8a9fec0d01038742d0cbd
Mateusz Kornecki <mateusz.kornecki@gmail.com>
2021-06-20 20:23:20 +0200

    fix: fixed app layout on mobiles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,30 +2,46 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { Navigation } from '../components/Navigation';
 import React from 'react';
 import { Container, Grid, makeStyles } from '@material-ui/core';
-import styles from '../styles/Home.module.css';
 import '../styles/globals.css';
 
 const queryClient = new QueryClient();
 
+const useStyles = makeStyles({
+	container: {
+		height: '100%',
+	},
+	grid: {
+		height: '100%',
+	},
+});
+
 function MyApp({ Component, pageProps }) {
+	const styles = useStyles();
+
+	React.useEffect(() => {
+		// Remove the server-side injected CSS.
+		const jssStyles = document.querySelector('#jss-server-side');
+		if (jssStyles) {
+			jssStyles.parentElement.removeChild(jssStyles);
+		}
+	}, []);
+
 	return (
 		<QueryClientProvider client={queryClient}>
-			<Container maxWidth='sm'>
-				<div className={styles.container}>
-					<Grid
-						container
-						direction='column'
-						justify='space-between'
-						alignItems='stretch'
-						className={styles.muiGrid}
-						wrap='nowrap'
-					>
-						<Component {...pageProps} />
-						<Grid item>
-							<Navigation />
-						</Grid>
+			<Container maxWidth='sm' className={styles.container}>
+				<Grid
+					container
+					direction='column'
+					justify='space-between'
+					alignItems='stretch'
+					className={styles.grid}
+					wrap='nowrap'
+				>
+					<Component {...pageProps} />
+					<Grid item>
+						<Navigation />
 					</Grid>
-				</div>
+				</Grid>
 			</Container>
 		</QueryClientProvider>
 	);
